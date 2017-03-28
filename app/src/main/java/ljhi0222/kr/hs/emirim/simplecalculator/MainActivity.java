be4f6f8d17c94941d14931849fbfdc8815645876
appcreate8 <appcreate8@appcreate8ui-iMac.local>
2017-03-28 20:12:45 +0900

    #1_2 layout and eventProgramming

--- app/src/main/java/ljhi0222/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/ljhi0222/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -2,12 +2,59 @@ package ljhi0222.kr.hs.emirim.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit_first;
+    EditText edit_second;
+    TextView text_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_first=(EditText)findViewById(R.id.edit_first);
+        edit_second=(EditText)findViewById(R.id.edit_second);
+        Button but_plus=(Button)findViewById(R.id.but_plus);
+        Button but_minus=(Button)findViewById(R.id.but_minus);
+        Button but_multiply=(Button)findViewById(R.id.but_multiply);
+        Button but_division=(Button)findViewById(R.id.but_division);
+        text_result=(TextView)findViewById(R.id.text_result);
+
+        but_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1=Integer.parseInt(edit_first.getText().toString())+Integer.parseInt(edit_second.getText().toString());
+                text_result.setText(""+num1);
+            }
+        });
+
+        but_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1=Integer.parseInt(edit_first.getText().toString())-Integer.parseInt(edit_second.getText().toString());
+                text_result.setText(""+num1);
+            }
+        });
+
+        but_multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1=Integer.parseInt(edit_first.getText().toString())*Integer.parseInt(edit_second.getText().toString());
+                text_result.setText(""+num1);
+            }
+        });
+
+        but_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1=Integer.parseInt(edit_first.getText().toString())/Integer.parseInt(edit_second.getText().toString());
+                text_result.setText(""+num1);
+            }
+        });
     }
 }
